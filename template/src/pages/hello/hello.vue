f9f06c68b415cf36067c4aba96dd8f18faade80c
qintianxiang <txqin@wisedu.com>
2017-03-21 19:46:04 +0800

    refine lib import

--- template/src/pages/hello/hello.vue
+++ template/src/pages/hello/hello.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='hello'>
-        <el-button @click.native="visible = true">按钮</el-button>
+        <el-button @click.native="send">点我~</el-button>
         <el-dialog v-model="visible" title="Hello world">
           <p>欢迎使用 Element</p>
         </el-dialog>
@@ -8,10 +8,22 @@
 </template>
 
 <script>
+    import {Button, Dialog} from 'element-ui';
+
+    Vue.use(Button);
+    Vue.use(Dialog);
+
+    const _send = function() {
+        this.visible = true;
+    };
+
     export default {
         data: () => ({
             visible: false
-        })
+        }),
+        methods: {
+            send: _send
+        }
     };
 </script>
 
